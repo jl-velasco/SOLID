28cc2556aa58a8564cad1033b9dec6d1b8c32eb8
Jorge <jorge.zgz@gmail.com>
2023-06-05 17:58:27 +0200

    Add Law of Demeter

---
+++ Demeter/Problem.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types = 1);
+
+class Car
+{
+    public function __construct(
+        private readonly Engine $engine,
+    )
+    {
+    }
+
+    public function power(): int
+    {
+        return $this->engine->power()->value();
+    }
+}
+
+class Engine
+{
+    public function __construct(private readonly Power $power)
+    {
+    }
+
+    public function power(): Power
+    {
+        return $this->power;
+    }
+}
+
+class Power
+{
+    public function __construct(private readonly int $power)
+    {
+    }
+
+    public function value(): int
+    {
+        return $this->power;
+    }
+}
